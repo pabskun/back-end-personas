ff11bd72134f487f274cd81418d648e200098638
Pabs Monestel <pablo.monestel@gmail.com>
2026-02-17 02:53:38 -0600

    cambio de puerto
    Se modifico el valor del puerto

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/personas", personaRoutes);
 
 // Puerto
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
